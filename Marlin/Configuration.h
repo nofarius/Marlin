3f3359c9082a761137755ab2bfcb2cf9d1d8236e
Sam Craig <sam@gigobin.com>
2022-10-15 21:39:33 +0800

    Setting PID valies

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -660,9 +660,9 @@
     #define DEFAULT_Ki_LIST {   1.54,   1.54 }
     #define DEFAULT_Kd_LIST {  76.55,  76.55 }
   #else
-    #define DEFAULT_Kp  21.73
-    #define DEFAULT_Ki   1.54
-    #define DEFAULT_Kd  76.55
+    #define DEFAULT_Kp  30.51
+    #define DEFAULT_Ki   2.96
+    #define DEFAULT_Kd  78.72
   #endif
 #endif
 
@@ -727,7 +727,7 @@
  * the issues involved, don't use bed PID until someone else verifies that your hardware works.
  * @section bed temp
  */
-//#define PIDTEMPBED
+#define PIDTEMPBED
 
 //#define BED_LIMIT_SWITCHING
 
@@ -743,9 +743,9 @@
   //#define MIN_BED_POWER 0
   //#define PID_BED_DEBUG // Print Bed PID debug data to the serial port.
 
-  #define DEFAULT_bedKp 50.71
-  #define DEFAULT_bedKi 9.88
-  #define DEFAULT_bedKd 173.43
+  #define DEFAULT_bedKp 122.95
+  #define DEFAULT_bedKi  24.01
+  #define DEFAULT_bedKd 419.67
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
